b840ae1343312c965253afc9cfee7348cca2a385
Shnurok1342 <a13bepb@yandex.ru>
2021-03-01 13:58:37 +0300

    (lessons 10) feat: Building Components with RxJs - Reactive Design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from '../model/course';
-import {noop} from 'rxjs';
+import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -11,25 +11,26 @@ import {createHttpObservable} from '../common/util';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
-  advancedCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
   constructor() {}
 
   ngOnInit() {
     const http$ = createHttpObservable('/api/courses');
-    const courses$ = http$
+    const courses$: Observable<Course[]> = http$
       .pipe(
         map(res => Object.values(res['payload']))
       );
 
-    courses$.subscribe(
-      (courses: Course[]) => {
-        this.beginnerCourses = courses.filter(c => c.category === 'BEGINNER');
-        this.advancedCourses = courses.filter(c => c.category === 'ADVANCED');
-        },
-      noop,
-      () => console.log('completed')
-    );
+    this.beginnerCourses$ = courses$
+      .pipe(
+        map(courses => courses.filter(c => c.category === 'BEGINNER'))
+      );
+
+    this.advancedCourses$ = courses$
+      .pipe(
+        map(courses => courses.filter(c => c.category === 'ADVANCED'))
+      );
   }
 }
