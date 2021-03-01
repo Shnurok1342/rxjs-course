f72f8171e43e65483c47d345bc5a53c482178232
Shnurok1342 <a13bepb@yandex.ru>
2021-03-01 18:17:43 +0300

    (lessons 32) feat: BehaviorSubject Store - Example of a Data Modification Operation

---
+++ src/app/common/store.service.ts
@@ -0,0 +1,62 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Course} from '../model/course';
+import {createHttpObservable} from './util';
+import {map, tap} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/internal-compatibility';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+  private subject = new BehaviorSubject<Course[]>([]);
+
+  courses$: Observable<Course[]> = this.subject.asObservable();
+
+  constructor() {
+  }
+
+  init() {
+    const http$ = createHttpObservable('/api/courses');
+    http$
+      .pipe(
+        tap(() => console.log('HTTP request executed')),
+        map(res => Object.values(res['payload']))
+      )
+      .subscribe(courses => this.subject.next(courses));
+  }
+
+  selectBeginnerCourses() {
+    return this.filterByCategory('BEGINNER');
+  }
+
+  selectAdvancedCourses() {
+    return this.filterByCategory('ADVANCED');
+  }
+
+  filterByCategory(category: string) {
+    return this.courses$
+      .pipe(
+        map(courses => courses
+          .filter(course => course.category === category))
+      );
+  }
+
+  saveCourse(courseId: number, changes): Observable<any> {
+    const courses = this.subject.getValue();
+    const courseIndex = courses.findIndex(course => course.id === courseId);
+    const newCourses = [...courses];
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes
+    };
+    this.subject.next(newCourses);
+    return fromPromise(fetch(`/api/courses/${courseId}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }));
+  }
+}
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,54 +1,49 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {AfterViewInit, Component, ElementRef, Inject, ViewChild} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Course} from '../model/course';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import {StoreService} from '../common/store.service';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: 'app-course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css']
 })
 export class CourseDialogComponent implements AfterViewInit {
+  form: FormGroup;
+  course: Course;
 
-    form: FormGroup;
+  @ViewChild('saveButton', {static: true}) saveButton: ElementRef;
+  @ViewChild('searchInput', {static: true}) searchInput: ElementRef;
 
-    course:Course;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    private storeService: StoreService,
+    @Inject(MAT_DIALOG_DATA) course: Course
+  ) {
+    this.course = course;
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required]
+    });
+  }
 
-    @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
+  ngAfterViewInit() {
+  }
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+  save() {
+    this.storeService.saveCourse(this.course.id, this.form.value)
+      .subscribe(
+        () => this.close(),
+        err => console.log('Error saving course:', err)
+      );
+  }
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
-
-        this.course = course;
-
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
-
-    }
-
-    ngAfterViewInit() {
-
-
-
-    }
-
-
-
-    close() {
-        this.dialogRef.close();
-    }
-
-
+  close() {
+    this.dialogRef.close();
+  }
 }
